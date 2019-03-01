c9e91733b53e11fe0a71292f46a034f98e5cfa94
Todd Morningstar <todd@thoughtbot.com>
2019-02-28 18:58:56 -0800

    Implement basic pattern matching with case class :mag:

---
+++ chapter3-5-3.scala
@@ -0,0 +1,13 @@
+/**
+ * Pattern matching with simple case classes
+ * Ch3.5.3.1
+ */
+case class Cats(name: String, color: String, food: String)
+
+object ChipShop {
+  def willServe(cat: Cat): Boolean =
+    cat match {
+      case Cat(_, _, "Chips") => true
+      case Cat(_, _, _) => false
+    }
+}
