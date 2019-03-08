6f4a8c93866f3137dcec4aff3834cddff18dbf43
Todd Morningstar <todd@thoughtbot.com>
2019-03-08 15:41:20 -0800

    Implement Pattern Matching More Time :mag:

--- chapter3-5-3.scala
+++ chapter3-5-3.scala
@@ -1,13 +1,26 @@
 /**
- * Pattern matching with simple case classes
- * Ch3.5.3.1
- */
+  * Pattern matching with simple case classes
+  * Ch3.5.3.1
+  */
 case class Cat(name: String, color: String, food: String)
 
 object ChipShop {
   def willServe(cat: Cat): Boolean =
     cat match {
       case Cat(_, _, "Chips") => true
-      case Cat(_, _, _) => false
+      case Cat(_, _, _)       => false
+    }
+}
+
+/**
+  * Pattern matching to derive a value associated with `Films` and `Directors`.
+  * Ch3.5.3.2
+  */
+object Dad {
+  def rate(film: Film): Double =
+    film match {
+      case Film(_, _, _, Director("Clint", "Eastwood", _)) => 10.0
+      case Film(_, _, _, Director("John", "McTiernan", _)) => 7.0
+      case _                                               => 3.0
     }
 }
