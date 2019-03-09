db7e4a9de454fa23364e48b2e022f354cd623b0b
Todd Morningstar <todd@thoughtbot.com>
2019-03-08 16:14:02 -0800

    Implement Shape Trait :cyclone:
    * Ch 4.1.2

--- ch4/chapter4-1.scala
+++ ch4/chapter4-1.scala
@@ -41,3 +41,31 @@ trait BigCat extends Feline {
 case class Smilodon(color: String, fangSize: Int) extends BigCat
 case class SnoopLion(color: String, currentStrain: String) extends BigCat
 case class JagPanther(color: String, clawSize: Int) extends BigCat
+
+/**
+  * Shape Traits
+  * Ch4.1.4.2
+  */
+trait Shape {
+  def sides: Int
+  def perimeter: Double
+  def area: Double
+}
+
+case class Circle(radius: Double) extends Shape {
+  val sides = 1
+  val perimeter = 2 * math.Pi * radius
+  val area = math.Pi * radius * radius
+}
+
+case class Rectangle(height: Double, width: Double) extends Shape {
+  val sides = 4
+  val perimeter = 2 * width + 2 * height
+  val area = width * height
+}
+
+case class Square(size: Double) extends Shape {
+  val sides = 4
+  val perimeter = 4 * size
+  val area = size * size
+}
