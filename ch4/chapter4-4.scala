8696aa300dc8380b1d0163b796d0a0a83f90753d
Todd Morningstar <todd@thoughtbot.com>
2019-03-15 15:56:44 -0700

    Demonstrate More Algebraic Data Types :recycle:
    * Moar sum type pattern
    * A Produkt type patturn

--- ch4/chapter4-4.scala
+++ ch4/chapter4-4.scala
@@ -1,5 +1,5 @@
 /**
-  * Demonstrate the sum type ("is a, or") pattern.
+  * Demonstrate the sum type ("is a, or") pattern. ➕
   * Case objects, rather than classes, are used since there are no fields or
   * methods on these types.
   * Ch4.4.4.1
@@ -10,10 +10,21 @@ case object Green extends TrafficLight
 case object Yellow extends TrafficLight
 
 /**
-  * Demonstrate the sum type pattern - again!
+  * Demonstrate the sum type pattern - again! ⚜
   * This time its all fancypants with arrrrrrghs.
   * Ch4.4.4.2
   */
 sealed trait Calculation
 final case class Success(result: Int) extends Calculation
 final case class Failure(reason: String) extends Calculation
+
+/**
+  * Product type ("has-a, and") pattern. ✖
+  * Its made up of a few things!
+  * Ch4.4.4.3
+  */
+sealed trait Source
+case object Well extends Source
+case object Spring extends Source
+case object Tap extends Source
+final case class BottledWater(size: Int, source: Source, carbonated: Boolean)
