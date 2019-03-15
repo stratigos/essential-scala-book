ca664a329d3a2b490b90d745dd02d9c623376801
Todd Morningstar <todd@thoughtbot.com>
2019-03-15 15:33:15 -0700

    Do more things with sum type pattern :1234:

--- ch4/chapter4-4.scala
+++ ch4/chapter4-4.scala
@@ -8,3 +8,12 @@ sealed trait TrafficLight
 case object Red extends TrafficLight
 case object Green extends TrafficLight
 case object Yellow extends TrafficLight
+
+/**
+  * Demonstrate the sum type pattern - again!
+  * This time its all fancypants with arrrrrrghs.
+  * Ch4.4.4.2
+  */
+sealed trait Calculation
+final case class Success(result: Int) extends Calculation
+final case class Failure(reason: String) extends Calculation
