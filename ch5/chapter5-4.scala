dec32960a6e10425541289a51c71ba5cd77c1ce0
Todd Morningstar <todd@thoughtbot.com>
2019-04-05 14:10:19 -0700

    Implement a "This or That" Generic Product Type :left_right_arrow:

--- ch5/chapter5-4.scala
+++ ch5/chapter5-4.scala
@@ -7,3 +7,15 @@
   * Ch5.4.1.1
   */
 case class SimplePair[A, B](one: A, two: B)
+
+/**
+  * Implement a trait Sum[A, B] with two subtypes Left and Right.
+  * Create type parameters so that Left and Right can wrap up values of
+  * two different types.
+  *
+  * Ch5.4.3.1
+  */
+trait Sum[A, B]
+
+case class Left[A, B](value: A) extends Sum
+case class Right[A, B](value: B) extends Sum
